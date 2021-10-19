d7c74807288f686261eb461766b8233e26c7192b
sreekanthzzz <75710924+sreekanthzzz@users.noreply.github.com>
2021-10-19 11:02:18 +0530

    commit

--- News/urls.py
+++ News/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static Import serve
-from django.conf.urls Import urls
+from django.views.static import serve
+from django.conf.urls import urls
 
 admin.autodiscover()
 urlpatterns = [
